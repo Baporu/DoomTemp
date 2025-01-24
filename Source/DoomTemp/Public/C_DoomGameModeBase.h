de0c5f13d6e7025a1b4df11725448b5d43418bd2
D0YUN <191944956+D0YUN@users.noreply.github.com>
2025-01-24 16:46:47 +0900

    [도윤] 이니셜 파일, General 파일 제작 / .gitignore 수정

---
+++ Source/DoomTemp/Public/C_DoomGameModeBase.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "C_DoomGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class DOOMTEMP_API AC_DoomGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
