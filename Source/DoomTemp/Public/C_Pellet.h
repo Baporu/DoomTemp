9475932a267ce491c8ca6b2afe1a841bee3efd16
Baporu <vitaqin@naver.com>
2025-02-13 19:52:43 +0900

    [호승] 안 올린 거 마저 올리기

---
+++ Source/DoomTemp/Public/C_Pellet.h
@@ -0,0 +1,23 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "C_ProjectileBullet.h"
+#include "C_Pellet.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class DOOMTEMP_API AC_Pellet : public AC_ProjectileBullet
+{
+	GENERATED_BODY()
+	
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:
+	void ApplySpread(float InSpread);
+};
